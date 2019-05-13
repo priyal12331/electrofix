a3dd3509f41b013ff67b84f460eda9fdee40f048
priyal12331 <50575562+priyal12331@users.noreply.github.com>
2019-05-13 14:46:04 +0530

    Add files via upload

---
+++ Untitled213.c
@@ -0,0 +1,69 @@
+//creating the bst tree
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+int data;
+struct node*left;
+struct node*right;
+
+
+}*node;
+struct node*insert(struct node*,int);
+struct node*display(struct node*);
+int main()
+{
+node=insert(node,80);
+insert(node,60);
+insert(node,40);
+insert(node,20);
+insert(node,10);
+display(node);
+
+}
+struct node*insert(struct node*node,int num)
+{
+struct node*temp;
+temp=(struct node*)malloc(sizeof(struct node));
+temp->data=num;
+temp->left=temp->right=NULL;
+if(node==NULL)
+{
+node=temp;
+return node;
+
+
+}
+else if(node->data>num)
+{
+
+node->left=insert(node->left,num);
+
+}
+else if(node->data<num)
+{
+node->right=insert(node->right,num);
+
+}
+
+
+
+
+
+
+
+
+}
+struct node*display(struct node*node)
+{
+if(node){display(node->left);
+printf("%d",node->data);
+display(node->right);}
+
+
+
+
+
+
+
+}
